1c34c46491ca566a420e15d20ce014319b07bd15
Syafaa765 <liaamalya91@gmail.com>
2020-10-14 13:54:37 +0700

    upload projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderHaneeda/>
+    <HeroHaneeda/>
+    <WomenHaneeda/>
+    <InstaHaneeda/>
+    <PartnerHaneeda/>
+    <FooterHaneeda/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderHaneeda from '@/components/HeaderHaneeda.vue';
+import HeroHaneeda from '@/components/HeroHaneeda.vue';
+import WomenHaneeda from '@/components/WomenHaneeda.vue';
+import InstaHaneeda from '@/components/InstaHaneeda.vue';
+import PartnerHaneeda from '@/components/PartnerHaneeda.vue';
+import FooterHaneeda from '@/components/FooterHaneeda.vue';
+
+export default {
+  name: 'Home',
+  components: {
+  HeaderHaneeda,
+  HeroHaneeda,
+  WomenHaneeda,
+  InstaHaneeda,
+  PartnerHaneeda,
+  FooterHaneeda
+  
+  }
+}
+</script>
